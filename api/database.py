3a448d2b6b0efc645ca2a878f6cbe51a0c3c2fb1
JC Catilo <catilo.johncarlo07@gmail.com>
2024-08-15 09:26:13 +0800

    Update database.py

--- api/database.py
+++ api/database.py
@@ -6,13 +6,14 @@ from dotenv import load_dotenv
 import os
 load_dotenv(dotenv_path="gms_configs.env")
 DATABASE_URL = os.getenv("DATABASE_URL")
-
+print("in database.py")
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
 try:
+    print("trying to connect to db")
     with engine.connect() as connection:
         print("Connection successful!")
 except Exception as e:
