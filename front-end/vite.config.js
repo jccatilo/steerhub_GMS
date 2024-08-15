5ac45c55a22c63af4e7dae6a41178008290c9370
roblestyle <133630940+roblestyle@users.noreply.github.com>
2024-08-15 09:03:49 +0800

    added multiple pages

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -10,5 +10,14 @@ export default {
   server: {
     port: 8080,
     hot: true
-  }
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'src/pages/index.html'),
+        page1: path.resolve(__dirname, 'src/pages/page1.html'),
+        page2: path.resolve(__dirname, 'src/pages/page2.html'),
+      },
+    },
+  },
 }
